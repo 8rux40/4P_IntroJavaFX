6665386acd86a21f23fec81c63099bedfaa44717
Bruno Tardin <brunotardin20@gmail.com>
2019-03-20 19:51:57 -0300

    Commit inicial

---
+++ src/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author btardin
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        /* Arquivo que vai carregar a tela da aplicação */
+        Parent root = FXMLLoader.load(getClass().getResource("Tela1.fxml"));
+        
+        /* Controlador de componentes */
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
